8045f43d6cd7af23fd7ad8abe494e2219c8edd6e
raghavbahukhandi <raghapb34n@gmail.com>
2022-06-01 13:04:48 +0530

    variables

--- variables/variables3.rs
+++ variables/variables3.rs
@@ -1,10 +1,12 @@
 // variables3.rs
 // Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 3;
+    // add 'mut' keyword to make a variable mutable
+    // let  mut y = 5; 
+    // y += 3;
+    // println!("Y is {}", y);
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
--- variables/variables5.rs
+++ variables/variables5.rs
@@ -1,11 +1,17 @@
 // variables5.rs
 // Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
-    let number = "T-H-R-E-E"; // don't change this line
+    let mut number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3;
-    println!("Number plus two is : {}", number + 2);
+    // below is a block that has a scope of its own and hence can have another variable named number
+    {
+        let number = 3;
+        println!("Number plus two is : {}", number + 2);
+    }
+    
+    // the varible `number` is still a string in this scope 
+    number = "F-I-V-E"; 
+    println!("This is spelt as: {}", number);
 }
