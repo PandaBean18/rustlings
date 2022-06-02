f24d63d19c10c918aa9bae8b89f1675bdc7b368a
raghavbahukhandi <raghapb34n@gmail.com>
2022-06-02 21:31:19 +0530

    modules

--- modules/modules1.rs
+++ modules/modules1.rs
@@ -1,15 +1,15 @@
 // modules1.rs
 // Make me compile! Execute `rustlings hint modules1` for hints :)
 
-// I AM NOT DONE
-
 mod sausage_factory {
+    // ALL functions and methods in rust are private by default, use `pub` keyword to make them public
+
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
